0dc8d8342042a99c50a07747377ca59e3c5b2138
Chris Smith <42503795+chrissmith012@users.noreply.github.com>
2019-12-01 16:04:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const pug = require('pug');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 // Setup express
 app.set('view engine', 'pug');
@@ -15,12 +17,29 @@ const PORT = process.env.PORT || 3000;
 const perPage = process.env.PAGE_SIZE || 10;
 //const defaultPassword = process.env.HACKERLOG_PASSWORD || 'P@ssw0rd!';
 
-app.get('/', function(req, res) {
-    res.render('index');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(PORT, ()=> {
     console.log(`server is up on port: ${PORT}`)
 });
 
+io.on('connection', function(socket){
+  console.log(username + ' connected');
+  socket.on('disconnect', function(){
+    console.log(username + ' disconnected');
+  });
+});
 
+io.on('connection', function(socket){
+  socket.broadcast.emit('Hello!');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    if(msg.length != 0){
+      io.emit('chat message', username + ": " + msg);
+    }
+  });
+});
