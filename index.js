c65cfdf5514a5fa73d287124546e33bfca712305
Chris Smith <42503795+chrissmith012@users.noreply.github.com>
2019-12-01 16:51:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const pug = require('pug');
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-// Setup express
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Get env variables
-const PORT = process.env.PORT || 3000;
-const perPage = process.env.PAGE_SIZE || 10;
-//const defaultPassword = process.env.HACKERLOG_PASSWORD || 'P@ssw0rd!';
-
-app.get('/', function(req, res) {
-    res.render('index');
+var username = 'chris';
+var schoolname = 'Rutgers University';
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(PORT, ()=> {
-    console.log(`server is up on port: ${PORT}`)
+io.on('connection', function(socket){
+  console.log(username + ' connected');
+  socket.on('disconnect', function(){
+    console.log(username + ' disconnected');
+  });
 });
 
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+  socket.broadcast.emit('Hello!');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    if(msg.length != 0){
+      io.emit('chat message', username + " [" + schoolname+ "] : " + msg);
+    }
+  });
+});
