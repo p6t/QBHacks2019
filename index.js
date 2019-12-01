ebfb1f6d22b3ca4faf50d3987b2a644946f1a8fb
ersatz-mentat <42197230+ersatz-mentat@users.noreply.github.com>
2019-12-01 14:57:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
 // Setup express
 app.set('view engine', 'ejs');
@@ -19,23 +17,7 @@ const perPage = process.env.PAGE_SIZE || 10;
 //const defaultPassword = process.env.HACKERLOG_PASSWORD || 'P@ssw0rd!';
 
 app.get('/', function(req, res) {
-    res.render('index.ejs');
-});
-
-io.sockets.on('connection', function(socket) {
-    socket.on('username', function(username) {
-        socket.username = username;
-        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
-    });
-
-    socket.on('disconnect', function(username) {
-        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
-    })
-
-    socket.on('chat_message', function(message) {
-        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
-    });
-
+    res.render('index');
 });
 
 const server = http.listen(port, function() {
