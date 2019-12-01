e36bbb3cfc516615a8370cd55a83a8510c4e7ddf
ersatz-mentat <42197230+ersatz-mentat@users.noreply.github.com>
2019-12-01 17:55:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('public'));
 app.get("/",(req,res)=>{
-  //res.send("Hello World!");
+  res.send("Hello World!");
   res.render('login');
 })
 app.use('/', login);
