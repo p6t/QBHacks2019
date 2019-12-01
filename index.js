1131c22e9288babd4f94b031488f0255431d36a1
aths01 <58394416+aths01@users.noreply.github.com>
2019-12-01 13:22:40 -0500

    Update index.js
    hhh

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const app = express();
 
 // Setup express
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+//app.set('views', __dirname + '/views');
+//app.use(express.static(__dirname + '/public'));
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 
 // Get env variables
 const port = process.env.PORT || 3000;
 const perPage = process.env.PAGE_SIZE || 10;
-const defaultPassword = process.env.HACKERLOG_PASSWORD || 'P@ssw0rd!';
+//const defaultPassword = process.env.HACKERLOG_PASSWORD || 'P@ssw0rd!';
 
 app.get('/', (req, res) => {
     //res.send('Whas poppin Jimbo??');
