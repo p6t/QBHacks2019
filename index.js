d8150d37147e109a17b7fb0a9a620f55bb381b85
Chris Smith <42503795+chrissmith012@users.noreply.github.com>
2019-12-01 16:08:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')();
 const bodyParser = require('body-parser');
-const app = express();
+const app = require('express')();
 const pug = require('pug');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
