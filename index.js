a45ccb953d14c2b8b7dd92a5d5a36a945078b1d9
ersatz-mentat <42197230+ersatz-mentat@users.noreply.github.com>
2019-12-01 14:51:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 // Setup express
 app.set('view engine', 'ejs');
@@ -16,10 +18,27 @@ const port = process.env.PORT || 3000;
 const perPage = process.env.PAGE_SIZE || 10;
 //const defaultPassword = process.env.HACKERLOG_PASSWORD || 'P@ssw0rd!';
 
-app.get('/', (req, res) => {
-    //res.send('Whas poppin Jimbo??');
-    //console.log('done did it!');
-    res.render('chatroom');
-})
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+
+io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+    });
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    })
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
+
+});
+
+const server = http.listen(port, function() {
+    console.log('listening on *:' + port);
+});
 
