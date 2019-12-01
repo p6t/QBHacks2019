567016ff2fb329d297fdbd22c48ae6221feef1ed
ersatz-mentat <peteretilton@gmail.com>
2019-12-01 11:36:29 -0500

    h

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//server init
+app.use(express.static('public'));
+
+//test get
 app.get('/', (req, res) => {
     res.send('Whas poppin Jimbo??');
     console.log('done did it!');
-})
+});
 
+//opening webapp
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
-})
+});
