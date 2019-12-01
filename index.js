d6248fa4ac9f79520b440b3c35a97d48e74af2f9
Chris Smith <42503795+chrissmith012@users.noreply.github.com>
2019-12-01 15:53:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,31 @@ app.get('/', function(req, res) {
 app.listen(PORT, ()=> {
     console.log(`server is up on port: ${PORT}`)
 });
+
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+var username = 'chris';
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log(username + ' connected');
+  socket.on('disconnect', function(){
+    console.log(username + ' disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.broadcast.emit('Hello!');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    if(msg.length != 0){
+      io.emit('chat message', username + ": " + msg);
+    }
+  });
+});
